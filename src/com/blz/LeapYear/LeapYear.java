7e143e3e292816d1c47d4d1cc8a1ee91828e89c1
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-10 11:21:52 +0530

    Added: Year is LeapYear Or Not Logic

--- src/com/blz/LeapYear/LeapYear.java
+++ src/com/blz/LeapYear/LeapYear.java
@@ -1,9 +1,28 @@
 package com.blz.LeapYear;
-
+import java.util.Scanner;
 public class LeapYear {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to LeapYear Program!!");
+		  int year;
+		  
+		System.out.println("Enter the Year:");
+		Scanner scanner = new Scanner(System.in);
+		year = scanner.nextInt();
+		
+		if(year % 100 == 0 && year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
+		{
+			System.out.println("It is a LeapYear");
+		}
+		
+		else
+		{
+			System.out.println("It is not a LeapYear");
+		}
+		  
+		
+		
+		
 
 	}
 
